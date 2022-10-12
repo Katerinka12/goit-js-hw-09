935ada3154fe6c29a1da2590c1b91b336cfb0e0f
Katerinka12 <katerinka.kk12@gmail.com>
2022-10-12 12:44:35 +0300

    final 1

--- dist/01-color-switcher.52e3b619.js
+++ dist/01-color-switcher.52e3b619.js
@@ -506,10 +506,22 @@ function hmrAcceptRun(bundle, id) {
 const startBtn = document.querySelector("button[data-start]");
 const endBtn = document.querySelector("button[data-stop]");
 endBtn.disabled = true;
+let timer = null;
 function getRandomHexColor() {
     return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
-startBtn.addEventListener("click", getRandomHexColor);
+startBtn.addEventListener("click", ()=>{
+    startBtn.disabled = true;
+    endBtn.disabled = false;
+    timer = setInterval(()=>{
+        document.body.style.backgroundColor = getRandomHexColor();
+    }, 1000);
+});
+endBtn.addEventListener("click", ()=>{
+    startBtn.disabled = false;
+    endBtn.disabled = true;
+    clearInterval(timer);
+});
 
 },{}]},["gzrwE","cYUEh"], "cYUEh", "parcelRequired7c6")
 
