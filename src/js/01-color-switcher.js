2f87757bcf15934bd20fae691fcc50aa7cf6c1ba
Katerinka12 <katerinka.kk12@gmail.com>
2022-10-12 12:20:25 +0300

    add

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,11 @@
 
+const startBtn = document.querySelector('button[data-start]');
+const endBtn = document.querySelector('button[data-stop]');
+endBtn.disabled = true;
+
+
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  }
+
+  startBtn.addEventListener('click', getRandomHexColor);
